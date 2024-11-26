b65a7161057d103ea6ff0a3cd144b1822d3f1169
cahmarinho <162758966+cahmarinho@users.noreply.github.com>
2024-11-25 23:39:04 -0300

    Delete Server directory

--- Server/src/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
